9052c12040f5af93bca51041a8fdb8ae764703b0
Aurelien <Aurelien@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-28 17:45:22 +0000

    First version of Display Template Name
    git-svn-id: https://plugins.svn.wordpress.org/display-template-name/trunk@549914 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ display-template-name.php
@@ -0,0 +1,81 @@
+<?php
+/*
+Plugin Name: Display Template Name
+Description: Displays the name of the template used by the currently displayed page. Plugins very useful for developing your blog.
+Version: 1.0
+Author: Aurélien Chappard
+Author URI: http://www.deefuse.fr/
+License: GPL
+Copyright: Aurélien Chappard
+*/
+if( !class_exists( 'Display_template_name' ) ) {
+	class Display_template_name {
+		public function __construct() {
+			load_plugin_textdomain('display-template-name', false, basename(dirname(__FILE__)).'/lang' );
+			
+		}
+		
+		/**
+		* Display the name on the top of the pages
+		*/
+		function displayTheTemplateName()
+		{
+			// Get the actual user
+			$actualUser =  wp_get_current_user();
+			if($actualUser->data != null)
+			{
+				// Css position according to the showing admin bar
+				$top = ( is_admin_bar_showing() ? "29px" : "0px" );
+				?>
+					<style type="text/css">
+						#debug-display-template-name{font-size: 14px; position: fixed; top: <?php echo $top;?>; left: 0px; background: #000; color: #FFF; padding: 5px; border: 1px solid #FFF; -moz-box-shadow: 5px 5px 5px 0px #CCCCCC;-webkit-box-shadow: 5px 5px 5px 0px #CCCCCC;-o-box-shadow: 5px 5px 5px 0px #CCCCCC;box-shadow: 5px 5px 5px 0px #CCCCCC;}
+					</style>
+					 <div id="debug-display-template-name"><?php _e('Current template:','display-template-name'); ?> <?php echo $this->get_current_template();?></div>
+				<?php
+			}
+			else
+			{
+				echo "prout";
+			}
+		}
+		
+		/**
+		* Retrieve the current template name et save into a global variable current_theme_template
+		*/
+
+		function var_template_include( $t )
+		{
+		    $GLOBALS['current_theme_template'] = basename($t);
+		    return $t;
+		}
+		
+		/**
+		* Grab the global variable et return it
+		*/		
+		function get_current_template( $echo = false )
+		{
+		    if( !isset( $GLOBALS['current_theme_template'] ) )
+		        return false;
+		    if( $echo )
+		        echo $GLOBALS['current_theme_template'];
+		    else
+		        return $GLOBALS['current_theme_template'];
+		}
+
+	}
+}
+
+
+if (class_exists("Display_template_name")) {
+	$display_template_name_plugin = new Display_template_name();
+}
+
+//Actions and Filters	
+if (isset($display_template_name_plugin))
+{
+	//Actions
+	add_action( 'wp_footer', array(&$display_template_name_plugin, 'displayTheTemplateName') );
+	
+	// Filter
+	add_filter( 'template_include', array(&$display_template_name_plugin,'var_template_include'), 1000 );
+}
---
+++ readme.txt
@@ -0,0 +1,22 @@
+=== Display Template Name ===
+Tags: developpement
+Requires at least: 3.0.0
+Tested up to: 3.3.1
+Stable tag: 1.0
+
+
+== Description ==
+
+Displays the name of the template used by the currently displayed page. Plugins very useful for developing your blog.
+
+
+== Installation ==
+
+1. Upload `display-template-name` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+
+== Screenshots ==
+1. No setting : activate and it'done !
