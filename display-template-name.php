302eed3c1a2a0eb006f791c8cc6ade4368aeea15
Aurelien <Aurelien@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-08 15:36:42 +0000

    change version
    git-svn-id: https://plugins.svn.wordpress.org/display-template-name/tags/1.6@1130511 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- display-template-name.php
+++ display-template-name.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: Display Template Name
 Description: Displays the name of the template used by the currently displayed page in the admin bar or inside the pages. Plugins very useful for developing your blog.
-Version: 1.5
-Author: Aurélien Chappard
+Version: 1.6
+Author: Aur§lien Chappard
 Author URI: http://www.deefuse.fr/
 License: GPL
 Copyright: Aurélien Chappard
